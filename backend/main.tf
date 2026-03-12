693b64cec56e647c73a3073f2396bf97a7338037
Apoorva R <apoorvarameshac@gmail.com>
2026-03-12 16:19:27 +0530

    Initial commit - Terraform EKS infrastructure

---
+++ backend/main.tf
@@ -0,0 +1,28 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
+
+
+resource "aws_s3_bucket" "example" {
+  bucket = "demo-terraform-eks-apoorva-state-bucket-1"
+
+  lifecycle {
+    prevent_destroy = false
+  }
+  
+}
+
+resource "aws_dynamodb_table" "basic-dynamodb-table" {
+  name           = "terraform-eks-state-locks"
+  billing_mode   = "PAY_PER_REQUEST"
+  hash_key       = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
+
+
+
